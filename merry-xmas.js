3e89474409f02bbfe31574fdf15988fbaa55edd6
BeccaStaple <56489791+BeccaStaple@users.noreply.github.com>
2019-12-23 10:48:07 +0000

    finished card

---
+++ merry-xmas.js
@@ -0,0 +1,66 @@
+
+
+//xmas letter click and close
+
+function openSec1() {
+    document.getElementById("sec1Id").style.display = "block"
+    document.getElementById("sec2Id").style.display = "none"
+    document.getElementById("sec3Id").style.display = "none"
+    document.getElementById("sec4Id").style.display = "none"
+}
+
+function closeSec1() {
+    document.getElementById("sec1Id").style.display = "none"
+    document.getElementById("sec2Id").style.display = "none"
+    document.getElementById("sec3Id").style.display = "none"
+    document.getElementById("sec4Id").style.display = "none"
+}
+
+
+//zero waste click & close
+
+function openSec2() {
+    document.getElementById("sec1Id").style.display = "none"
+    document.getElementById("sec2Id").style.display = "block"
+    document.getElementById("sec3Id").style.display = "none"
+    document.getElementById("sec4Id").style.display = "none"
+}
+
+function closeSec2() {
+    document.getElementById("sec1Id").style.display = "none"
+    document.getElementById("sec2Id").style.display = "none"
+    document.getElementById("sec3Id").style.display = "none"
+    document.getElementById("sec4Id").style.display = "none"
+}
+
+//fun GIF of xmas shot gun
+
+function openSec3() {
+    document.getElementById("sec1Id").style.display = "none"
+    document.getElementById("sec2Id").style.display = "none"
+    document.getElementById("sec3Id").style.display = "block"
+    document.getElementById("sec4Id").style.display = "none"
+}
+
+function closeSec3() {
+    document.getElementById("sec1Id").style.display = "none"
+    document.getElementById("sec2Id").style.display = "none"
+    document.getElementById("sec3Id").style.display = "none"
+    document.getElementById("sec4Id").style.display = "none"
+}
+
+//final message
+
+function openSec4() {
+    document.getElementById("sec1Id").style.display = "none"
+    document.getElementById("sec2Id").style.display = "none"
+    document.getElementById("sec3Id").style.display = "none"
+    document.getElementById("sec4Id").style.display = "block"
+}
+
+function closeSec4() {
+    document.getElementById("sec1Id").style.display = "none"
+    document.getElementById("sec2Id").style.display = "none"
+    document.getElementById("sec3Id").style.display = "none"
+    document.getElementById("sec4Id").style.display = "none"
+}
